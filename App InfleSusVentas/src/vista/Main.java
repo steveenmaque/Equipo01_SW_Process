e970332f0d6cd56bb00bfcc85ff5006bac372e45
Steveen Maque <steveen.maque@unmsm.edu.pe>
2025-11-09 18:38:12 -0500

    Agregando estructura inicial src y Main.java

---
+++ App InfleSusVentas/src/vista/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("¡Sistema InfleSusVentas iniciando...");
+        // Aquí llamaremos a tu primera ventana más adelante
+        // new vista.Login().setVisible(true);
+    }
+}
