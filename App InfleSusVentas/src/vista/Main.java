6379ddb03e8fc26b8c45efa25e13618308879722
steveenmaque <steveen.maque@unmsm.edu.pe>
2025-11-09 20:41:12 -0500

    Delete App InfleSusVentas/src/vista directory

--- App InfleSusVentas/src/vista/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("¡Sistema InfleSusVentas iniciando...");
-        // Aquí llamaremos a tu primera ventana más adelante
-        // new vista.Login().setVisible(true);
-    }
-}
